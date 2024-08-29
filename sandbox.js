42a9ec3504097d26dbdf193f95a557391e681072
Alexey917 <pochtovii.1996@mail.ru>
2024-08-29 22:43:43 +0300

    Added new task

--- sandbox.js
+++ sandbox.js
@@ -1,42 +1,26 @@
-function f(a) {
-  console.log(a);
-}
+let hamster = {
+  stomach: [],
 
-function throttle(func, ms) {
-  let isThrottled = false,
-    savedArgs,
-    savedThis;
+  set eat(food) {
+    this.stomach.push(food);
+  },
 
-  function wrapper() {
-    if (isThrottled) {
-      // (2)
-      savedArgs = arguments;
-      savedThis = this;
-      return;
-    }
+  get eat() {
+    return this.stomach;
+  },
+};
 
-    func.apply(this, arguments); // (1)
+let speedy = {
+  __proto__: hamster,
+};
 
-    isThrottled = true;
+let lazy = {
+  __proto__: hamster,
+};
 
-    setTimeout(function () {
-      isThrottled = false; // (3)
-      if (savedArgs) {
-        wrapper.apply(savedThis, savedArgs);
-        savedArgs = savedThis = null;
-      }
-    }, ms);
-  }
+// Этот хомяк нашёл еду
+speedy.eat("apple");
+console.log(speedy.stomach); // apple
 
-  return wrapper;
-}
-
-// f1000 передаёт вызовы f максимум раз в 1000 мс
-let f1000 = throttle(f, 1000);
-
-f1000(1); // показывает 1
-f1000(2); // (ограничение, 1000 мс ещё нет)
-f1000(3); // (ограничение, 1000 мс ещё нет)
-
-// когда 1000 мс истекли ...
-// ...выводим 3, промежуточное значение 2 было проигнорировано
+// У этого хомяка тоже есть еда. Почему? Исправьте
+console.log(lazy.stomach); // apple
